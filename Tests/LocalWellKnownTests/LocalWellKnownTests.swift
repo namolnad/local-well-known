6d93bbee3d64d78cf90d148e3561e248a3842128
Dan Loman <daniel.h.loman@gmail.com>
2023-02-11 01:42:50 -0800

    Add test for json file case

--- Tests/LocalWellKnownTests/LocalWellKnownTests.swift
+++ Tests/LocalWellKnownTests/LocalWellKnownTests.swift
@@ -5,17 +5,19 @@ final class LocalWellKnownTests: XCTestCase {
     private let jsonEncoder = JSONEncoder()
 
     private var commands: [String] = []
+    private var file: String?
+    private var json: String?
     private var output: [String] = []
     private var port: UInt16?
     private var tunnelHost: String?
-    private var json: String?
 
     override func setUp() async throws {
         commands = []
-        output = []
-        port = nil
-        tunnelHost = nil
+        file = nil
         json = nil
+        port = nil
+        output = []
+        tunnelHost = nil
     }
 
     func testManualStrategy() async throws {
@@ -49,7 +51,7 @@ final class LocalWellKnownTests: XCTestCase {
             [
                 "ps -o pid -o command | grep -E \'^\\s*\\d+ ssh -R 80:localhost:8765 localhost.run\' | awk \"{print \\$1}\" | xargs kill",
                 "ssh-keygen -F localhost.run || ssh-keyscan -H localhost.run >> ~/.ssh/known_hosts",
-                "ssh -R 80:localhost:8765 localhost.run -- --output json"
+                "ssh -R 80:localhost:8765 localhost.run -- --output json",
             ]
         )
         XCTAssertEqual(output, ["", "Add com.blah to your app\'s entitlements file.", "\n"])
@@ -94,12 +96,103 @@ final class LocalWellKnownTests: XCTestCase {
                 "ssh -R 80:localhost:8765 localhost.run -- --output json",
                 "/usr/libexec/PlistBuddy -c \'set :com.apple.developer.associated-domains:0 applinks:com.blah\' ImAScheme/ImAScheme.entitlements || /usr/libexec/PlistBuddy -c \'add :com.apple.developer.associated-domains:0 string applinks:com.blah\' ImAScheme/ImAScheme.entitlements",
                 "/usr/libexec/PlistBuddy -c \'set :com.apple.developer.associated-domains:1 webcredentials:com.blah\' ImAScheme/ImAScheme.entitlements || /usr/libexec/PlistBuddy -c \'add :com.apple.developer.associated-domains:1 string webcredentials:com.blah\' ImAScheme/ImAScheme.entitlements",
-                "xcrun xcodebuild -quiet -showBuildSettings  -json -project \'hello.xcodeproj\' -scheme \'ImAScheme\' 2> /dev/null"
+                "xcrun xcodebuild -quiet -showBuildSettings  -json -project \'hello.xcodeproj\' -scheme \'ImAScheme\' 2> /dev/null",
             ]
         )
         XCTAssertEqual(output, [])
         XCTAssertEqual(port, 8765)
         XCTAssertEqual(tunnelHost, "com.blah")
         XCTAssertEqual(json, "{\"applinks\":[\"details\":[{\"appIds\":[\"team123.com.bundle.example\"]}],\"webcredentials\":{\"apps\":[\"team123.com.bundle.example\"]}")
+    }
+
+
+    func testWorkspaceFileStrategy() async throws {
+        Current.shell._run = { [unowned self] command in
+            self.commands.append(command)
+            return command.starts(with: "xcrun xcodebuild") ?
+                try self.jsonEncoder.encode(LocalWellKnown.BuildSettingsResponse.init(actionSettings: [.init(action: "build", buildSettings: .init(teamId: "team123", bundleId: "com.bundle.example"))])) :
+                .init()
+        }
+        Current.shell.runAsyncStream = { [unowned self] command in
+            self.commands.append(command)
+            return .init {
+                try self.jsonEncoder.encode(LocalWellKnown.SSHResponse(address: URL(string: "com.blah")!))
+            }
+        }
+        Current.server.run = { [unowned self] port, tunnelHost, json in
+            self.port = port
+            self.tunnelHost = tunnelHost
+            self.json = json
+        }
+        Current.stdout._write = { [unowned self] in
+            self.output.append($0)
+        }
+
+        try await LocalWellKnown.run(
+            strategy: .workspace(file: "hello.xcworkspace", scheme: "ImAScheme"),
+            port: 8765,
+            entitlementsFile: "ImAScheme/ImAScheme.entitlements"
+        ) { _ in }
+
+        XCTAssertEqual(
+            commands,
+            [
+                "ps -o pid -o command | grep -E \'^\\s*\\d+ ssh -R 80:localhost:8765 localhost.run\' | awk \"{print \\$1}\" | xargs kill",
+                "ssh-keygen -F localhost.run || ssh-keyscan -H localhost.run >> ~/.ssh/known_hosts",
+                "ssh -R 80:localhost:8765 localhost.run -- --output json",
+                "/usr/libexec/PlistBuddy -c \'set :com.apple.developer.associated-domains:0 applinks:com.blah\' ImAScheme/ImAScheme.entitlements || /usr/libexec/PlistBuddy -c \'add :com.apple.developer.associated-domains:0 string applinks:com.blah\' ImAScheme/ImAScheme.entitlements",
+                "/usr/libexec/PlistBuddy -c \'set :com.apple.developer.associated-domains:1 webcredentials:com.blah\' ImAScheme/ImAScheme.entitlements || /usr/libexec/PlistBuddy -c \'add :com.apple.developer.associated-domains:1 string webcredentials:com.blah\' ImAScheme/ImAScheme.entitlements",
+                "xcrun xcodebuild -quiet -showBuildSettings  -json -workspace \'hello.xcworkspace\' -scheme \'ImAScheme\' 2> /dev/null",
+            ]
+        )
+        XCTAssertEqual(output, [])
+        XCTAssertEqual(port, 8765)
+        XCTAssertEqual(tunnelHost, "com.blah")
+        XCTAssertEqual(json, "{\"applinks\":[\"details\":[{\"appIds\":[\"team123.com.bundle.example\"]}],\"webcredentials\":{\"apps\":[\"team123.com.bundle.example\"]}")
+    }
+
+    func testJsonFileStrategy() async throws {
+        Current.shell._run = { [unowned self] command in
+            self.commands.append(command)
+            return .init()
+        }
+        Current.shell.runAsyncStream = { [unowned self] command in
+            self.commands.append(command)
+            return .init {
+                try self.jsonEncoder.encode(LocalWellKnown.SSHResponse(address: URL(string: "com.blah")!))
+            }
+        }
+        Current.server.run = { [unowned self] port, tunnelHost, json in
+            self.port = port
+            self.tunnelHost = tunnelHost
+            self.json = json
+        }
+        Current.stdout._write = { [unowned self] in
+            self.output.append($0)
+        }
+        Current.contentsOfFile = { [unowned self] file in
+            self.file = file
+            return "{\"iamjson\":34}"
+        }
+
+        try await LocalWellKnown.run(
+            strategy: .json(file: "example.json"),
+            port: 123,
+            entitlementsFile: nil
+        ) { _ in }
+
+        XCTAssertEqual(
+            commands,
+            [
+                "ps -o pid -o command | grep -E \'^\\s*\\d+ ssh -R 80:localhost:123 localhost.run\' | awk \"{print \\$1}\" | xargs kill",
+                "ssh-keygen -F localhost.run || ssh-keyscan -H localhost.run >> ~/.ssh/known_hosts",
+                "ssh -R 80:localhost:123 localhost.run -- --output json",
+            ]
+        )
+        XCTAssertEqual(output, ["", "Add com.blah to your app\'s entitlements file.", "\n"])
+        XCTAssertEqual(port, 123)
+        XCTAssertEqual(tunnelHost, "com.blah")
+        XCTAssertEqual(json, "{\"iamjson\":34}")
+        XCTAssertEqual(file, "example.json")
     }
 }
